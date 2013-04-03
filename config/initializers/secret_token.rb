23c272f864131f0c7ddd34a289794c1b0d152b93
Jose Rivera <jriverac@me.com>
2013-04-03 08:27:52 -0600

    New blank application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Copywriter::Application.config.secret_token = '4e2bf04fe0bb06b9c993ec9c8d4b4be2b7796ac3cd7c307f6244e801d9c93de059ccc0ea881598509181d0e0954728c7252e7da74e706ffb4c99713e5ec97dfa'
